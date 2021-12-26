09dc6eedf7d576ab7961035a7e0ce387c79d7682
Anastasiya Zazerskaya <an_zazerskaya@mail.ru>
2021-12-26 19:14:14 +0300

    feat: add request and response logging

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
 import Router from 'koa-router';
+import { finished } from 'stream';
 
 const app = new Koa();
 const router = new Router();
@@ -19,9 +20,28 @@ router.get(
 
 app.use(koaBody());
 
+app.use(async (ctx, next) => {
+  const { url, method, querystring, body } = ctx.request;
+  const bodyReQ = body;
+  await next();
+  finished(ctx.res, () => {
+    const { status, body } = ctx.response;
+    console.log(
+      `METHOD: ${method}
+       URL: ${url}
+       QUERY: ${querystring || 'no query'}
+       BODY REQUEST: ${JSON.stringify(bodyReQ)}
+       STATUS: ${status}
+       BODY RESPONSE: ${JSON.stringify(body)} `
+    );
+  });
+});
+
 app.use(userRouter.routes());
 app.use(boardRouter.routes());
 app.use(taskRouter.routes());
 app.use(router.routes());
 
 export default app;
+
+
