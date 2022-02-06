6f2eafb93fbc91d6edd4775509796e3ecebcaec3
Anastasiya Zazerskaya <an_zazerskaya@mail.ru>
2022-02-06 23:22:42 +0300

    feat: add usage of fastify or express depending on USE_FASTIFY

--- nest-js-api/src/main.ts
+++ nest-js-api/src/main.ts
@@ -1,15 +1,20 @@
 import {  NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 config();
 
 
 const PORT = process.env.PORT || 7000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app: INestApplication | NestFastifyApplication;
+  process.env.USE_FASTIFY ?  app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  ) : app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(PORT);
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
